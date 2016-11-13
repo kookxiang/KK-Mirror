408b4d23a095f77027379263861e07f1cf10ff60
kookxiang <kookxiang@gmail.com>
2016-11-14 02:08:47 +0800

    Fix version compare func

--- module/project/index.jsx
+++ module/project/index.jsx
@@ -6,6 +6,32 @@ import { ProgressBar } from "react-toolbox/lib/progress_bar";
 import { List, ListItem } from "react-toolbox/lib/list";
 import { browserHistory } from "react-router";
 
+function version_compare(v1, v2) {
+    var vm = { "dev": -6, "alpha": -5, "a": -5, "beta": -4, "b": -4, "RC": -3, "rc": -3, "#": -2, "p": 1, "pl": 1 };
+    var _prepVersion = function (v) {
+        v = ("" + v).replace(/[_\-+]/g, ".");
+        v = v.replace(/([^.\d]+)/g, ".$1.").replace(/\.{2,}/g, ".");
+        return (!v.length ? [-8] : v.split("."));
+    };
+    var _numVersion = function (v) {
+        return !v ? 0 : (isNaN(v) ? vm[v] || -7 : parseInt(v, 10));
+    };
+    v1 = _prepVersion(v1);
+    v2 = _prepVersion(v2);
+    let x = Math.max(v1.length, v2.length);
+    for (let i = 0; i < x; i++) {
+        if (v1[i] === v2[i])  continue;
+        v1[i] = _numVersion(v1[i]);
+        v2[i] = _numVersion(v2[i]);
+        if (v1[i] < v2[i]) {
+            return -1;
+        } else if (v1[i] > v2[i]) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 export class Project extends React.Component {
     constructor(props) {
         super(props);
@@ -26,23 +52,7 @@ export class Project extends React.Component {
         fetch(`/${this.state.folderName}/metadata.json`)
             .then(response => response.json())
             .then(data => {
-                data.versions = data.versions.sort((left, right) => {
-                    if (typeof left.version + typeof right.version != "stringstring")
-                        return false;
-                    var a = left.version.split("."), b = right.version.split("."), i = 0, len = Math.max(a.length, b.length);
-                    for (; i < len; i++) {
-                        if ((a[i] && !b[i] && parseInt(a[i]) > 0) || (parseInt(a[i]) > parseInt(b[i]))) {
-                            return -1;
-                        } else if ((b[i] && !a[i] && parseInt(b[i]) > 0) || (parseInt(a[i]) < parseInt(b[i]))) {
-                            return 1;
-                        } else if (a[i] > !b[i]) {
-                            return -1;
-                        } else if (a[i] < !b[i]) {
-                            return 1;
-                        }
-                    }
-                    return 0;
-                });
+                data.versions = data.versions.sort((left, right) => -version_compare(left.version, right.version));
                 this.setState({ loading: false, project: data });
             }).catch(() => {
                 Snackbar.Show({
